d90c09cf390e25e7c5452378bc074fb6b96d6901
eracool007 <viacarolinefontaine@gmail.com>
2022-05-05 10:44:55 -0400

    Titre

--- index.php
+++ index.php
@@ -16,12 +16,12 @@ $choosenPic = rand(0, $numberOfPictures-1);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tic Tac Toe</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600&display=swap"
       rel="stylesheet" />
